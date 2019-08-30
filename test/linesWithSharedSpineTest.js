280083ad490a58e3c63fe14e8c3db95d1a0bf7be
KateCourt <nkc124@ncl.ac.uk>
2019-08-30 11:13:07 +0100

    lines with shared spine tests

--- test/linesWithSharedSpineTest.js
+++ test/linesWithSharedSpineTest.js
@@ -5,14 +5,16 @@ const sharedSpine = require('../util/linesWithSharedSpine.js').sharedSpine;
 describe('shared spine', function() {
    it('should return correct list', function() {
        const matching = sharedSpine('B.31');
-       console.log(matching);
+       assert.strictEqual(matching[0], 'P2.35');
+       assert.strictEqual(matching[1], 'P3.35');
+       assert.strictEqual(matching.length, 2);
    });
     it('should return empty list where there is only one line for the spine index', function() {
         const matching = sharedSpine('P2.18');
-        console.log(matching);
+        assert.strictEqual(matching.length, 0);
     });
     it('should return undefined when the line does not exist', function() {
         const matching = sharedSpine('B2.31');
-        console.log(matching);
+        assert.ok(matching === undefined);
     });
 });
