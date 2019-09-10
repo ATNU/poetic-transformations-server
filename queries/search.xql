f83f552b6d11e25db4f60212b856afce7e1f3ccb
KateCourt <nkc124@ncl.ac.uk>
2019-09-10 12:13:41 +0100

    search puts each text in <text> tag

--- queries/search.xql
+++ queries/search.xql
@@ -1,15 +1,19 @@
+
 xquery version "3.1";
 import module namespace kwic="http://exist-db.org/xquery/kwic";
 declare namespace tei= "http://www.tei-c.org/ns/1.0";
 
-for $resource in collection('/db/transformations')//tei:TEI[ft:query(., '"So would there be migration"')]
+for $resource in collection('/db/transformations')//tei:TEI[ft:query(., '"arms"')]
 let $path := base-uri($resource)
 let $name := replace($path, '/db/transformations/', '')
+let $result :=kwic:summarize($resource, <config width='100'/>)
 return  (
-    <title> {$resource//tei:title/text()} </title>,
-    <author> {$resource//tei:author/text()} </author>,
-    <poemID> {$resource//tei:idno[@type='PTpoem']/text()} </poemID>,
-    <versionID> {$resource//tei:idno[@type='PTid']/text()} </versionID>,
-    <filename>($name)</filename>,
-    kwic:summarize($resource, <config width='100'/>)
+    <text>
+        <title> {$resource//tei:title/text()} </title>,
+        <author> {$resource//tei:author/text()} </author>,
+        <poemID> {$resource//tei:idno[@type='PTpoem']/text()} </poemID>,
+        <versionID> {$resource//tei:idno[@type='PTid']/text()} </versionID>,
+        <filename>{$name}</filename>,
+        <results>{$result}</results>,
+    </text>
 )
--- routes/search.js
+++ routes/search.js
@@ -11,19 +11,8 @@ router.get('/:searchTerm', function(req, res) {
     //----- generate database API call
     const search = req.params.searchTerm;
 
-//xquery version and TEI namespace
-    const querySetup = "xquery version '3.1';import module namespace kwic='http://exist-db.org/xquery/kwic';declare default element namespace 'http://www.tei-c.org/ns/1.0';";
-    const queryStatement = "for $resource in collection('/db/transformations')//TEI[ft:query(.," + "'" + search + "')]" +
-        "let $path := base-uri($resource)" +
-        "let $name := replace($path, '/db/transformations/', '')" +
-   " return  (" +
-        "<title> {$resource//title/text()} </title>," +
-        "<author> {$resource//author/text()} </author>," +
-        "<poemID> {$resource//idno[@type='PTpoem']/text()} </poemID>," +
-    "<versionID> {$resource//idno[@type='PTid']/text()} </versionID>," +
-    "<filename>{$name}</filename>," +
-    "kwic:summarize($resource, <config width='100'/>))&_howmany=100";
-    const query = querySetup + queryStatement;
+
+    const query = "xquery version '3.1';import module namespace kwic='http://exist-db.org/xquery/kwic';declare namespace tei= 'http://www.tei-c.org/ns/1.0';for $resource in collection('/db/transformations')//tei:TEI[ft:query(., '" + search +"')] let $path := base-uri($resource) let $name := replace($path, '/db/transformations/', '') let $result :=kwic:summarize($resource, <config width='100'/>) return  ( <text><title> {$resource//tei:title/text()} </title><author> {$resource//tei:author/text()} </author> <poemID> {$resource//tei:idno[@type='PTpoem']/text()} </poemID>  <versionID> {$resource//tei:idno[@type='PTid']/text()} </versionID> <filename>{$name}</filename><results>{$result}</results></text>)&_howmany=100";
     // full address to call
     const URL = encodeURI(URI + query);
     console.log(URL);
