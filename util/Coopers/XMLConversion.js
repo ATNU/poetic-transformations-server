972e8b17532148d3f68278f35cc5d6365a9bf841
katecourt <nkc124@ncl.ac.uk>
2021-02-03 09:44:55 +0000

    add xml field to json line object list

---
+++ util/Coopers/XMLConversion.js
@@ -0,0 +1,132 @@
+const xmlToJson = require('xml-js');
+const _ = require ('underscore');
+
+/**
+ * Convert supplied xml to a string representation of a JSON object
+ * @param xml
+ * @returns {string}
+ */
+function parseXml(xml) {
+    try {
+        const json= xmlToJson.xml2json(xml, {compact: true, spaces: 4});
+        return JSON.parse(json);
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+
+/**
+ * Extract line objects from a JSON representation of the xml
+ * @param json (string)
+ * @returns lines (JSON)
+ */
+function getLines(json) {
+    try {
+        // const parsed = JSON.parse(json);
+        // todo cope with lines that contain a tag at the beginning rather than leaving these as undefined
+        const lines = json["TEI"]["text"]["body"]["div"]["lg"];
+        return lines;
+    } catch(err) {
+        console.log(err);
+    }
+
+}
+
+/**
+ * Extract key information from a JSON representation of the poem body. Each line is made up of a line ID (xml:id) and text (_text)
+ * @param lines
+ * @returns {Array}
+ */
+const idTextMap = (lines) => {
+    const map = [];
+
+    //each text block
+    for (let k = 0; k < lines.length ; k++) {
+        const block = lines[k]["l"];
+
+        //each line object
+        for (let m = 0; m < block.length; m++) {
+            const line = block[m];
+            const xmlId = line._attributes["xml:id"];
+            const text = line._text;
+
+            // recreate xml for this line to preserve
+            const xmlObject = {
+                l: line
+            }
+            const xml = xmlToJson.json2xml(xmlObject, {compact: true, ignoreComment: true, spaces: 4})
+
+            // create new line object
+            const newLine = {"xml:id": xmlId, "_text": text, "xml": xml};
+            map.push(newLine);
+
+        }
+    }
+
+    return map;
+};
+
+/**
+ * Extract only the text from the poem as one long string (no punctuation or undefined sections)
+ * @param xml
+ * @returns {string}
+ */
+const justText = xml => {
+    const parsed = parseXml(xml);
+    const lines = getLines(parsed);
+
+    let text = "";
+
+    //each text block
+    for (let k = 0; k < lines.length ; k++) {
+        const block = lines[k]["l"];
+
+        //each line object
+        for (let m = 0; m < block.length; m++) {
+            const line = block[m];
+            let sentence = line._text;
+            let sentenceString;
+            //can't deal with lines that are undefined
+            if (sentence === undefined) {
+                continue;
+            }
+
+            //if sentence is a list break into a string and remove punctuation
+            if (_.isArray(sentence)) {
+                sentenceString = processList(sentence);
+            }
+            //otherwise just remove punctuation
+            else {
+                sentenceString = sentence.replace( /[^\w\s]/g, "");
+            }
+
+            text = text + " " + sentenceString;
+        }
+    }
+    return text.replace(/undefined/g, "");
+};
+
+//break up lines that appear as lists (happens where there are internal tags)
+const processList = list => {
+    let wholeSentence;
+
+    for (let i = 0; i < list.length; i++) {
+        const part = list[i];
+
+        if (part === undefined) {
+            continue;
+        }
+
+        const noPunc = part.replace( /[^\w\s]/g, "");
+        wholeSentence = wholeSentence + " " + noPunc;
+    }
+    return wholeSentence;
+};
+
+
+module.exports.parseXml = parseXml;
+module.exports.getLines = getLines;
+module.exports.idTextMap = idTextMap;
+module.exports.justText = justText;
---
+++ util/Coopers/compareToA.js
@@ -0,0 +1,71 @@
+// const xmlConversion = require('../util/Coopers/XMLConversion.js').justText;
+const lines = require('./XMLConversion').getLines;
+const fs = require('fs');
+const map = require('./XMLConversion').idTextMap;
+const parse = require('./XMLConversion').parseXml;
+const archetypeName = 'P1.xml'
+const versionToCompareName = 'M1.xml'
+const xmlToJson = require('xml-js');
+
+function getLines() {
+
+    let mappedList = [];
+
+
+        //get xml and convert
+        const xml = fs.readFileSync("../../data/M1.xml");
+        const mapped = map(lines(parse(xml)));
+        mappedList.push(mapped);
+
+    console.log(mappedList);
+
+
+    //get xml and convert
+//     const xml = ' <lg>\n' +
+//         '               <l xml:id="M5.2">But what was Pakistan? a better life.</l>\n' +
+//         '               <l xml:id="M5.3"><del change="#change-b">New country</del>, a bolt-hole,<metamark\n' +
+//         '                     change="#change-b" rend="caret" function="unclear"/> a road of bone?</l>\n' +
+//         '               <!-- Changed caret to metamark rend caret -> should it be <g>? -->\n' +
+//         '            </lg>'
+// let lines = parse(xml)
+//     lines = lines['lg']['l']
+//     //each text block
+//     for (let k = 0; k < lines.length ; k++) {
+//         const line = lines[k];
+//             const xmlId = line._attributes["xml:id"];
+//             const text = line._text
+//         const xmlObject = {
+//                 l: line
+//         }
+//
+//         const xml = xmlToJson.json2xml(xmlObject, {compact: true, ignoreComment: true, spaces: 4})
+//             const newLine = {"xml:id": xmlId, "_text": text, "xml": xml};
+// console.log(newLine)
+
+
+
+
+
+    // var options = {compact: true, ignoreComment: true, spaces: 4};
+    // const backToXML = xmlToJson.json2xml(ls, options);
+    // console.log('test' + backToXML);
+//
+// //each text block
+//     for (let k = 0; k < ls.length ; k++) {
+//         const block = ls[k]["l"];
+//
+//         //each line object
+//         for (let m = 0; m < block.length; m++) {
+//             const line = block[m];
+//
+//             const xmlId = line._attributes["xml:id"];
+//             const text = line._text;
+//
+//             const newLine = {"xml:id": xmlId, "_text": text};
+//
+//
+//         }
+//     }
+}
+
+getLines()
